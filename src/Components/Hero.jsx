8bbacb96b0cd5ba219b05d7e59b38dc888de110a
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-02 14:00:28 +0000

    clear chat alignment-simple-centre

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,6 +5,7 @@ import {React, useState} from 'react'
 const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form Submited");
+    demoChatFunc()
 }
 
 
@@ -20,19 +21,28 @@ function QueryForm () {
     );
 }
 
-async function demoChatFunc () {
-    
-}
+
 
 function Hero() {
+    
+    let response = JSON.parse('{"user": "Hi","KhushiSRT": "How can I help you today?","user": "Hi","KhushiSRT": "How can I help you today?"}');
+    console.log(response)
 
-    let [chat, setChat] = useState('');
+    let chatArray = () => {
+        response.forEach(element => {
+            console.log()
+        });
+    }
+    
+    let [chat, setChat] = useState(response.KhushiSRT);
+
+
 
   return (
     <>
     <div id="container" className=' h-[85vh] bg-[#1E1E1E] flex flex-col w-full justify-center items-center gap-5' >
-        <div id="dialogueBox" className='h-[50vh] w-[80vw] bg-[#2e2e2e] rounded-2xl overflow-y-scroll'>
-            {chat}
+        <div id="dialogueBox" className='h-[50vh] w-[80vw] bg-[#2e2e2e] rounded-2xl overflow-y-scroll flex flex-col justify-center items-center'>
+            <p className="text-xl text-white">{chat}</p>
         </div>
     <QueryForm />
     </div>
