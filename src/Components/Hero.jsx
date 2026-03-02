67ab9f98f9aeb0685b38e625167a3432f4c8f77e
lucky-KRMU <2501730142@krmu.edu.in>
2026-03-02 13:30:51 +0000

    add query form

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,33 @@
 import {React, useState} from 'react'
 
+
+
+
+
+
+function QueryForm () {
+    return (
+        <>
+        <form className='w-[80vw] h-[10vh] bg-[#2e2e2e] rounded-2xl flex flex-row justify-center items-center gap-5'>
+            <input type="text" name="query" id="query" className="w-[70vw] font-[play]  text-3xl text-white bg-transparent focus:outline-0" placeholder='Shall we start talking ... ? 😁'/>
+            <input type="submit" value="&uarr;" className='bg-[#3e3e3e] text-white h-10 w-10 text-2xl rounded-4xl cursor-pointer' />
+
+        </form>
+        </>
+    );
+}
+
 function Hero() {
 
     let [chat, setChat] = useState('');
 
   return (
     <>
-    <div id="container" className=' h-[85vh] bg-[#1E1E1E] flex flex-col w-full justify-center items-center' >
+    <div id="container" className=' h-[85vh] bg-[#1E1E1E] flex flex-col w-full justify-center items-center gap-5' >
         <div id="dialogueBox" className='h-[50vh] w-[80vw] bg-[#2e2e2e] rounded-2xl overflow-y-scroll'>
             {chat}
         </div>
+    <QueryForm />
     </div>
     </>
   )
